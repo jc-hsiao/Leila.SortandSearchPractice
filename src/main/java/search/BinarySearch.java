9e1806fa2bb9b5dbe10fa83662a03ced49291052
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-31 11:53:18 -0400

    finish prime

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -5,17 +5,29 @@ public class BinarySearch<T extends Comparable<T>>{
     //should return the index of the element found
     //return -1 if not found
     int search(T value, T[] array){
-        return BS(0,array.length,array,value);
+        return binarySearchGeneric(0,array.length,array,value);
     }
 
-    int BS(int left, int right, T[] arr, T val){
+    public boolean binarySearch(int left, int right, int value, int[] array){
         int mid = (left+right)/2;
-        if(left==right){
+
+        if( left>=right )
+            return false;
+        else if( array[mid] < value )
+            return binarySearch( mid+1, right, value, array );
+        else if( array[mid] > value )
+            return binarySearch( left, mid, value, array );
+        else return array[mid] == value;
+    }
+
+    public int binarySearchGeneric(int left, int right, T[] arr, T val){
+        int mid = (left+right)/2;
+        if(left>=right){
             return -1;
         }else if(arr[mid].compareTo(val) > 0  ){
-            return BS(left+1, mid, arr, val);
+            return binarySearchGeneric(left+1, mid, arr, val);
         }else if(arr[mid].compareTo(val) < 0 ){
-            return BS(mid, right, arr, val);
+            return binarySearchGeneric(mid, right, arr, val);
         }else if(arr[mid].compareTo(val) == 0 )
             return mid;
         return -1;
--- src/main/java/search/FindPrime.java
+++ src/main/java/search/FindPrime.java
@@ -1,5 +1,8 @@
 package search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindPrime {
 
     //Possible method layout
@@ -7,8 +10,23 @@ public class FindPrime {
     // - Return n primes, ascending, starting from 2
     // - Check if n is prime... etc
 
-    public int[] getPrime(int form, int to){
-        return null;
+    public int[] getPrime(int from, int to){
+        List<Integer> a = new ArrayList<>();
+        for (int i = from; i <= to ; i++) {
+            if(isPrime(i))
+                a.add(i);
+        }
+        return a.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 
+    public boolean isPrime(int num){
+        if(num<=2) return false;
+        for (int i = 2; i < num ; i++) {
+            if(num%i==0)
+                return false;
+        }
+        return true;
+    }
 }
